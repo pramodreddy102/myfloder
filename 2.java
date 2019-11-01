a6dd06bc89712cc0816f92cb55b941e732e1d099
pramodreddy102 <pramodreddy102@gmail.com>
2019-11-01 11:03:18 +0530

    Create 2.java

---
+++ 2.java
@@ -0,0 +1,9 @@
+lient version: 0.8.0
+Go version (client): go1.2
+Git commit (client): cc3a8c8
+Go version (client): go1.2
+Git commit (client): cc3a8cent version: 0.8.0
+Go version (client): go1.2
+Git commit (client): cc3a8c8
+Go version (client): go1.2
+Git commit (client): cc3a8c8
